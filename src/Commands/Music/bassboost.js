2b595616f12e21f7b0cc5994841f2846811c87bd
Daniel <72373873+Daniel12345rs01@users.noreply.github.com>
2021-09-27 18:12:17 +0200

    Update bassboost.js
    Danish

--- src/Commands/Music/bassboost.js
+++ src/Commands/Music/bassboost.js
@@ -18,14 +18,14 @@ module.exports = class bassboost extends Command {
         })
     }
     async run(message, args) {
-        if(message.channel.name != 'musik') return message.reply("Wrong channel mate.... Please use #musik");
+        if(message.channel.name != 'musik') return message.reply("Forkert kanal bror/søster brug #musik");
         const player = message.client.manager.get(message.guild.id);
-        if (!player) return message.reply("there is no player for this guild.");
+        if (!player) return message.reply("Der er ingen spiller i denne klub.");
 
         const { channel } = message.member.voice;
 
-        if (!channel) return message.reply("you need to join a voice channel.");
-        if (channel.id !== player.voiceChannel) return message.reply("you're not in the same voice channel.");
+        if (!channel) return message.reply("Du mangler at tilslutte en talekanal.");
+        if (channel.id !== player.voiceChannel) return message.reply("Du er ikke i den samme talekanal.");
 
         let level = "none";
         if (args.length && args[0].toLowerCase() in levels) level = args[0].toLowerCase();
@@ -38,6 +38,6 @@ module.exports = class bassboost extends Command {
 
         player.setEQ(...bands);
 
-        return message.reply(`set the bassboost level to ${level}`);
+        return message.reply(`Ændrer bassbost til level ${level}`);
     }
 };
